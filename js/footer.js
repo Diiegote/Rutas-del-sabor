480cbd19717e4ff7a0d742dae8050b6fa61c4911
Diiegote <diego.elprado22@gmail.com>
2023-05-16 19:16:45 -0300

    fixed bug

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', function() {
   var footerPlaceholder = document.getElementById('footer-placeholder');
   var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'footer.html', true);
+  xhr.open('GET', '../app/footer.html', true);
   xhr.onreadystatechange = function() {
      if (xhr.readyState === 4 && xhr.status === 200) {
         footerPlaceholder.innerHTML = xhr.responseText;
