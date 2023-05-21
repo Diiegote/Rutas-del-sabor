e36d13ca0b586f2c954cd34867f2dad4ccf8acf4
fabrisegat <fabrisegat03@gmail.com>
2023-05-21 16:12:45 -0300

    platos terminado

---
+++ js-api/detalle1.js
@@ -0,0 +1,21 @@
+const YOUR_API_KEY = "ea6d6cd8ff54453a9dd74ef4f46e49be";
+
+async function request() {
+  const resultado = (
+    await axios(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=10`
+    )
+    
+  ).data.results;
+
+//   const recetas = await resultado.map((a) => a[0]?.image);
+  const recetas = resultado[0].image;
+  console.log(resultado[0].image);
+  let imagesContainer = document.getElementById("detalle0");
+
+  let imageElement = document.createElement("img");
+    imageElement.src = recetas;
+    imagesContainer.appendChild(imageElement);
+  
+    }
+    request()
